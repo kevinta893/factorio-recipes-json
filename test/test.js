4569b0b83e43a8f3eccf0f238cbb1cb287966152
kevinta893 <kevinta893@gmail.com>
2018-02-23 15:16:45 -0700

    Fix Travis build error

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 //attempts to parse the recipes JSON file for syntax errors
 var fs = require('fs');
-var recipesJSON = JSON.parse(fs.readFileSync('../recipes.json', 'utf8'));			//synchronous
+var recipesJSON = JSON.parse(fs.readFileSync('./recipes.json', 'utf8'));			//synchronous
 console.log("Total Recipes found=" + recipesJSON.length);
 console.log("Testing done.");
