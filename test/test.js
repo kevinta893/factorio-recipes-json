dc9cf5f6f285d6368cf52348b79d9c20cded4e68
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-24 06:24:55 +0000

    Deploy kevinta893/factorio-recipes-json to github.com/kevinta893/factorio-recipes-json.git:gh-pages

--- test/test.js
+++ test/test.js
@@ -2,4 +2,65 @@
 var fs = require('fs');
 var recipesJSON = JSON.parse(fs.readFileSync('./recipes.json', 'utf8'));			//synchronous
 console.log("Total Recipes found=" + recipesJSON.length);
+
+
+var recipes = {};
+//Setup database in hash table
+for (var i = 0 ; i < recipesJSON.length ; i++){
+	var recipe = recipesJSON[i];
+	recipes[recipe.id] = recipe;
+}
+
+
+var DEPTH_CUTOFF = 10000;			
+
+
+for (var i = 0 ; i < recipesJSON.length ; i++){
+	console.log("Recursing on recipe=" + recipesJSON[i].id + 
+		", max_depth=" + recurseRecipe(recipesJSON[i].id));
+}
+
+
+//digs down on each item's recipe and see if it terminates
+function recurseRecipe(recipeId){
+	return recurseRecipeTree(recipeId, 0);
+}
+
+
+function recurseRecipeTree(recipeId, level){
+	
+	if (level >= DEPTH_CUTOFF){
+		throw new Error("Recursion for recipe too deep! id=" + recipeId);
+	}
+	
+	
+	var recipeItem = recipes[recipeId];
+
+    if (recipeItem.type == "raw"){
+        return level;
+    }
+    else{
+        var recipeItems = recipeItem.recipe0.items;
+		var maxdepth = level;
+        for (var i = 0; i < recipeItems.length ; i++) {
+            var recipePart = recipeItems[i];
+            //recurse per recipe part and combine results
+			maxdepth = Math.max(maxdepth, recurseRecipeTree(recipePart.id, level + 1));
+        }
+
+        return maxdepth;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
 console.log("Testing done.");
+
